5cb07926974b49f26159a8116634c165c33493b7
Shahbaaz Imam Khan <112297555+instinct1one@users.noreply.github.com>
2023-02-07 17:32:10 +0530

    UPDATED

--- copy.sh
+++ copy.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 ​
 #copying mvn settings.xml to /usr/share/maven/config
-sudo cp -rf ./settings.xml /usr/share/maven/conf/
+sudo cp -rf $WORKSPACE/settings.xml /usr/share/maven/conf/
 ​
 #copying tomcat-users.xml to /var/lib/tomcat9/conf
-sudo cp -rf ./tomcat-users.xml /var/lib/tomcat9/conf/
+sudo cp -rf $WORKSPACE/tomcat-users.xml /var/lib/tomcat9/conf/
