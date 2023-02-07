ba8a62eeefc62df610352ebfa46a196db74610d0
Shahbaaz Imam Khan <112297555+instinct1one@users.noreply.github.com>
2023-02-07 17:32:54 +0530

    UPDATED

--- copy_2.sh
+++ copy_2.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 #copying *.war to tomcat9/webapps
-sudo cp -r /home/ubuntu/onlinebookstore/target/*.war /var/lib/tomcat9/webapps/
+sudo cp -r $WORKSPACE/target/*.war /var/lib/tomcat9/webapps/
